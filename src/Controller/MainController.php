69ba6218e7e178ea83abc1d2381c60a4cd2ba0eb
melspence <melvillespence@hamnavoesolutions.com>
2026-06-09 14:54:08 +0100

    Added MainController

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('<strong>Starshop</strong>: your monopoly busting option for Starship parts!');
+    }
+}

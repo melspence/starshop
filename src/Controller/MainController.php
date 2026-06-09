f788a088032b437ce7c541b1e4be60ec3ffbada8
melspence <melvillespence@hamnavoesolutions.com>
2026-06-09 15:39:08 +0100

    updated hompage to display my ship

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,6 +13,18 @@ class MainController extends AbstractController
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly busting option for Starship parts!');
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-001)',
+            'class' => 'Garden',
+            'captain' => 'jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
